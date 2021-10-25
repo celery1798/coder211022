c0f6fe9c71d44ee11c319c2ff2a08f77ed1b8f59
celery1798 <celery1798@163.com>
2021-10-25 14:56:26 +0800

    new__mod_

---
+++ c/basic/io_test.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <math.h>
+
+#define WEIGHT		3.0e-23
+#define QUART		950
+
+void test1()
+{
+	float  q,total;
+
+	printf("Please enter:");
+	scanf("%f",&q);
+
+	total = q * QUART / WEIGHT;
+
+	printf("total = %e\n",total);
+}
+
+void test2()
+{
+	int a,b,c;
+	float area,s;
+
+	printf("Please enter:");
+	scanf("%d%d%d",&a,&b,&c);
+
+	if(a+b <= c || b+c <= a || a+c <= b)
+	{
+		printf("Input error.\n");
+		return ;
+	}
+
+	s = 1.0/2*(a+b+c);
+	area = sqrt(s * (s-a) * (s-b) * (s-c));
+	printf("area = %f\n",area);
+}
+
+void test3()
+{
+	float a,b,c,dis,x1,x2,p,q;
+
+	printf("Please enter:");
+	scanf("%f%f%f",&a,&b,&c);
+	
+	dis = b*b - 4*a*c;
+	/*if error*/	
+	p = -b/(2*a);
+	q = sqrt(dis)/(2*a);
+	x1 = p + q;
+	x2 = p - q;
+
+	printf("x1 = %f, x2 = %f\n",x1,x2);
+}
+
+int main()
+{
+
+//	test1();
+//	test2();
+	test3();
+	return 0;
+}
+
+
+
+
